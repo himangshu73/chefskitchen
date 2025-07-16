aa638a8af9b5d84665df03fa7cbe94b8a8c86d10
himangshu <himangshu73@gmail.com>
2025-07-16 08:34:19 +0600

    hereo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,22 +3,36 @@ import Button from "./Button";
 
 export default function Hero() {
   return (
-    <div className="flex py-20">
-      <div className="w-1/2">
-        <h2>Cook Anything with the experts.</h2>
-        <p>
+    <div className="flex items-center py-20 px-8">
+      <div className="w-1/2 space-y-6">
+        <h2>
+          Cook anything
+          <br /> with the experts.
+        </h2>
+        <p className="py-4">
           Sed ut perspiciantis unde omnis iste natus error sit oluptatem
           acusantium doloremque laudantium totam
         </p>
-        <div>
+        <div className="flex gap-4 pt-4">
           <Button>Let's Cook</Button>
           <Button variant="outline">Explore now</Button>
         </div>
       </div>
-      <div className="w-1/2">
-        <div>
-          <Image src="/shapes.png" alt="shapes" height={400} width={400} />
-        </div>
+      <div className="relative w-1/2 h-[536px] flex items-end justify-center">
+        <div className="absolute bottom-0 bg-primary w-[420px] h-[500px] rounded-t-full z-0"></div>
+        <Image
+          src="/shapes.png"
+          alt="floating dots"
+          fill
+          className="object-contain z-10"
+        />
+        <Image
+          src="/lady.png"
+          alt="lady cooking"
+          width={400}
+          height={536}
+          className="relative z-20"
+        />
       </div>
     </div>
   );
