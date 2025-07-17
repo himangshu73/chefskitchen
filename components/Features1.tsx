aa1749a801b721304f8dfa216cae9aee140eb1d8
himangshu <himangshu73@gmail.com>
2025-07-18 00:17:43 +0600

    features1 update

--- components/Features1.tsx
+++ components/Features1.tsx
@@ -4,32 +4,34 @@ import Button from "./Button";
 export default function Features1() {
   return (
     <div className="container mx-auto py-20">
-      <div className="">
-        <div className="relative">
+      <div className="flex items-center justify-between">
+        <div className="relative w-1/2 h-[536px]">
           <div className="absolute bottom-0 left-0 bg-secondary rounded-tr-full w-[424px] h-[536px]"></div>
           <div className="absolute bottom-0 left-0">
             <Image src="/man.png" width={424} height={492} alt="man" />
           </div>
         </div>
 
-        <div>
-          <div>
-            <p>COOK WITH US</p>
+        <div className="w-1/2 flex flex-col items-start">
+          <div className="flex flex-col gap-3 mb-3">
+            <p className="tracking-widest">COOK WITH US</p>
+            <h3>Cooking together with the expert</h3>
           </div>
-          <h3>Cooking together with the expert</h3>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem
-          </p>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium....
-          </p>
-          <Button variant="solid">Learn More</Button>
+          <div className="flex flex-col gap-10">
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem
+            </p>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium....
+            </p>
+            <Button variant="solid">Learn More</Button>
+          </div>
         </div>
       </div>
     </div>
