c1cba9af7320980f4b7b8edf78db5d1295acb405
himangshu <himangshu73@gmail.com>
2025-07-20 08:34:09 +0600

    res

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,8 @@ import Button from "./Button";
 export default function Hero() {
   return (
     <div className="bg-tertiary-15">
-      <div className="container mx-auto flex items-center py-20 px-4">
-        <div className="w-1/2 space-y-6">
+      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center py-8md:py-16 px-4 gap-8">
+        <div className="w-full md:w-1/2 text-center md:text-left space-y-6">
           <h2>
             Cook anything
             <br /> with the experts.
@@ -14,19 +14,19 @@ export default function Hero() {
             Sed ut perspiciantis unde omnis iste natus error sit oluptatem
             acusantium doloremque laudantium totam
           </p>
-          <div className="flex gap-4 pt-4">
+          <div className="flex flex-col md:flex-row justify-center md:justify-start gap-4 pt-4">
             <Button>Let's Cook</Button>
             <Button variant="outline">Explore now</Button>
           </div>
         </div>
-        <div className="relative w-1/2 h-[536px] flex items-end justify-center">
+        <div className="relative w-full md:w-1/2 max-w-[536px] h-auto aspect-[5/6] mx-auto">
           <Image
             src="/shapes.png"
             alt="floating dots"
             fill
             className="object-contain z-0"
           />
-          <div className="relative w-[450px] h-[536px]">
+          <div className="absolute inset-0 flex items-end justify-center">
             <div className="absolute bottom-0 right-0 bg-primary w-[380px] h-[500px] rounded-t-full z-0"></div>
             <Image
               src="/lady.png"
