f61f63e32adae8536f8b028656ca5ddd032055c2
himangshu <himangshu73@gmail.com>
2025-07-21 22:12:17 +0600

    res

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 export default function Hero() {
   return (
     <div className="bg-tertiary-15">
-      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center py-8md:py-16 px-4 gap-8">
+      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center px-4 md:px-8 py-12 md:py-20 gap-12">
         <div className="w-full md:w-1/2 text-center md:text-left space-y-6">
           <h2>
             Cook anything
@@ -19,7 +19,7 @@ export default function Hero() {
             <Button variant="outline">Explore now</Button>
           </div>
         </div>
-        <div className="relative w-full md:w-1/2 max-w-[536px] h-auto aspect-[5/6] mx-auto">
+        <div className="relative w-full md:w-1/2 max-w-md lg:max-w-lg h-auto aspect-[5/6] mx-auto">
           <Image
             src="/shapes.png"
             alt="floating dots"
@@ -27,13 +27,13 @@ export default function Hero() {
             className="object-contain z-0"
           />
           <div className="absolute inset-0 flex items-end justify-center">
-            <div className="absolute bottom-0 right-0 bg-primary w-[380px] h-[500px] rounded-t-full z-0"></div>
+            <div className="absolute bottom-0 right-1/2 translate-x-1/2 md:right-0 md:translate-0 bg-primary w-64 sm:w-80 h-[360px] sm:h-[500px] rounded-t-full z-10"></div>
             <Image
               src="/lady.png"
               alt="lady cooking"
-              width={450}
-              height={536}
-              className="absolute bottom-0 right-0 z-20"
+              width={300}
+              height={400}
+              className="absolute bottom-0 right-1/2 translate-x-1/2 md:right-0 md:translate-x-0 z-20"
             />
           </div>
         </div>
