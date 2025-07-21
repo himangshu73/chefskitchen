32df6f65e60233ab1b65c982ef20273ffc9018e7
himangshu <himangshu73@gmail.com>
2025-07-22 00:00:23 +0600

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import Chefs from "@/components/Chefs";
-import Features from "@/components/Features";
-import Features1 from "@/components/Features1";
-import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
-import Newsletter from "@/components/Newsletter";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <Features />
-      <Features1 />
-      <Chefs />
-      <Gallery />
-      <Newsletter />
     </div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,37 +4,31 @@ import Button from "./Button";
 export default function Hero() {
   return (
     <div className="bg-tertiary-15">
-      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center px-4 md:px-8 py-12 md:py-20 gap-12">
-        <div className="w-full md:w-1/2 text-center md:text-left space-y-6">
-          <h2>
-            Cook anything
-            <br /> with the experts.
-          </h2>
-          <p className="py-4">
-            Sed ut perspiciantis unde omnis iste natus error sit oluptatem
-            acusantium doloremque laudantium totam
-          </p>
-          <div className="flex flex-col md:flex-row justify-center md:justify-start gap-4 pt-4">
-            <Button>Let&apos;s Cook</Button>
-            <Button variant="outline">Explore now</Button>
-          </div>
-        </div>
-        <div className="relative w-full md:w-1/2 max-w-md lg:max-w-lg h-auto aspect-[5/6] mx-auto">
+      <div className="container mx-auto px-4 flex flex-col sm:flex-row-reverse">
+        <div className="relative w-full sm:w-1/2 aspect-5/6">
           <Image
             src="/shapes.png"
             alt="floating dots"
             fill
-            className="object-contain z-0"
+            className="object-contain"
           />
-          <div className="absolute inset-0 flex items-end justify-center">
-            <div className="absolute bottom-0 right-1/2 translate-x-1/2 md:right-0 md:translate-0 bg-primary w-64 sm:w-80 h-[360px] sm:h-[500px] rounded-t-full z-10"></div>
-            <Image
-              src="/lady.png"
-              alt="lady cooking"
-              width={300}
-              height={400}
-              className="absolute bottom-0 right-1/2 translate-x-1/2 md:right-0 md:translate-x-0 z-20"
-            />
+          <div>
+            <div className="absolute bottom-20 right-0 w-3/4 aspect-526/644 bg-primary rounded-t-full"></div>
+            <Image src="/lady.png" fill className="object-contain" alt="lady" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 sm:gap-8 sm:w-1/2 sm:mt-52">
+          <h2>
+            Cook anything <br /> with the experts.
+          </h2>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            <br />
+            accusantium doloremque laudantium, totam
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4">
+            <Button>Let&apos;s cook</Button>
+            <Button variant="outline">Explore now</Button>
           </div>
         </div>
       </div>
